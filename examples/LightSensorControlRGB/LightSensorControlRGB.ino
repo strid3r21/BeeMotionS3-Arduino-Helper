5dcc9f08f7eb3cc3412e3ecae218a82fc90169c4
Paul Price <56952812+strid3r21@users.noreply.github.com>
2022-08-28 14:55:17 -0400

    Update LightSensorControlRGB.ino

--- examples/LightSensorControlRGB/LightSensorControlRGB.ino
+++ examples/LightSensorControlRGB/LightSensorControlRGB.ino
@@ -1,4 +1,5 @@
-// This code shows how to use the Bee Motion S3 as a night light
+// This code shows how to use control the RGB brightness by the level of the ambiet light level.
+// the darker the ambient light, the brighter the RGB gets and vise versa.
 
 #include <BMS3.h> //include the bee motion s3 helper library
 
@@ -13,11 +14,11 @@ void setup() {
 }
 
 void loop() {
-  float lightLevel = bms3.getLight();
-  Serial.println(lightLevel);
-  int rgbLevel = map(lightLevel, .3, 1.2, 255, 0);
- bms3.setPixelBrightness(rgbLevel);
-bms3.setPixelColor(blue);
+  float lightLevel = bms3.getLight(); //get the ambient light level
+  Serial.println(lightLevel); // print the light level so we can reference it.
+  int rgbLevel = map(lightLevel, .3, 1.2, 255, 0); //map the light level input to a range between 255 - 0
+ bms3.setPixelBrightness(rgbLevel); // set the RGB brihtness to that mapped brightness level based on ambiet light level
+bms3.setPixelColor(blue); //turn on the RGB to Blue or whatever other color you'd like
 
   
 
